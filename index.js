aac64e91ff1b2615f66cbe111b81446e012c1408
hongbowu <wuhongbo87@gmail.com>
2023-12-15 17:05:34 -0500

    add questions

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const generateMarkdown = require("./generateMarkdown")
 const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //ask user to put project title
+    "What is your project's title?",
+    //ask user to put introduction
+    "Please give some introductions.",
+    //ask user to put user story
+    "You can add user story here.",
+    //ask user to put how to use it
+    "Can you explain how to use your application?",
+    //ask user the way to contact information
+    "Leave the contact information to collect feedbacks.",
+    //ask user what license will use or no license
+    "What license do you want to use?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
